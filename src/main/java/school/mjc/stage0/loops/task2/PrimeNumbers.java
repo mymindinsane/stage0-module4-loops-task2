528bf9ea6c5dec5769ab9306450869e7196ca639
mymindinsane <33089089+mymindinsane@users.noreply.github.com>
2024-06-11 16:15:31 +0300

    Update PrimeNumbers.java

--- src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
@@ -2,19 +2,22 @@ package school.mjc.stage0.loops.task2;
 
 public class PrimeNumbers {
     public void printPrimeNumbers(int printToInclusive) {
-        int counter = 2;
-        while (counter <= printToInclusive){
-            boolean isPrime = true;
-            double squareRoot = Math.sqrt(counter) + 2;
-            int r = 2;
-            while (r <= squareRoot && isPrime) {
-                if (counter % r == 0) isPrime = false;
-                r++;
-            }
-            if (isPrime){
-                System.out.println(counter);
-            }
-            counter++;
-        }
+   {
+    int counter = 2;
+    while (counter < printToInclusive){
+    int i = 2;
+    boolean flag = false;
+    while (i <= counter / 2) {
+      if (counter % i == 0) {
+        flag = true;
+        break;
+      }
+      ++i;
+    }
+    if (!flag)
+      System.out.println(counter);
+      counter++;
+    }
+
     }
 }
